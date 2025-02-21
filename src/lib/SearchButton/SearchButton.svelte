06b21b24b880eb8c29062dbef17449afa232a5cb
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-20 20:03:47 -0500

    updated styling of SearchButton

--- src/lib/SearchButton/SearchButton.svelte
+++ src/lib/SearchButton/SearchButton.svelte
@@ -2,4 +2,6 @@
 	export let onClick = () => {};
 </script>
 
-<button on:click={onClick}>Search</button>
+<button class="mt-4 rounded-xl bg-sky-200 p-4 text-xl hover:bg-sky-300" on:click={onClick}>
+	Search
+</button>
