6f9493f446ea008d223f33e9f97fdd0c70655a1d
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-20 23:19:25 -0500

    refactor - abstracted e2e test actions to functions to improve readability

--- e2e/search.test.ts
+++ e2e/search.test.ts
@@ -1,17 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('Search functionality on the page', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+const fillSearchInput = async (page, value) => {
+	const searchInput = page.locator('input[placeholder="Potatoes, carrots, beef..."]');
+	await searchInput.fill(value);
+	await expect(searchInput).toHaveValue(value);
+};
 
-  const searchInput = page.locator('input[placeholder="Potatoes, carrots, beef..."]');
-  await expect(searchInput).toBeVisible();
+const clickSearchButton = async (page) => {
+	const searchButton = page.locator('button:has-text("Search")');
+	await searchButton.click();
+};
 
-  await searchInput.fill('Potatoes');
-  await expect(searchInput).toHaveValue('Potatoes');
+const searchTest = async (page, searchTerm) => {
+	await page.goto('http://localhost:5173/');
+	await fillSearchInput(page, searchTerm);
+	await clickSearchButton(page);
+	await expect(page.locator('input[placeholder="Potatoes, carrots, beef..."]')).toHaveValue(
+		searchTerm
+	);
+};
 
-  const searchButton = page.locator('button:has-text("Search")');
-  await expect(searchButton).toBeVisible();
-  await searchButton.click();
-
-  await expect(searchInput).toHaveValue('Potatoes');
+test('Search functionality on the page with Carrots', async ({ page }) => {
+	await searchTest(page, 'Carrots');
 });
