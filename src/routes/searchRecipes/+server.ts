71f4cdcd89155044e699cdfc6d13f94cd754332f
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-22 15:44:43 -0500

    green test

--- src/routes/searchRecipes/+server.ts
+++ src/routes/searchRecipes/+server.ts
@@ -1,9 +1,10 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import {
 	extractRecipeIds,
-	parseIDs
+	constructBulkApiURL,
+	fetchBulkRecipeInformation,
+	filterInformationBulkReponse
 } from '$lib/server/searchUtils/recipeByIDs/recipeByIDsUtils.js';
-
 import {
 	parseIngredients,
 	fetchRecipeByIngredients,
@@ -11,23 +12,26 @@ import {
 } from '$lib/server/searchUtils/recipeByIngredients/recipeByIngredientsUtils.js';
 
 export const GET: RequestHandler = async ({ url }) => {
+	// 1. Parse ingredients from the request URL
 	const ingredientsOrResponse = parseIngredients(url);
 	if (ingredientsOrResponse instanceof Response) {
 		return ingredientsOrResponse;
 	}
 	const ingredients = ingredientsOrResponse;
 
+	// 2. Construct the API URL to search recipes by ingredients
 	const recipeByIngredientsUrl = constructApiUrl(ingredients);
 
+	// 3. Fetch recipes based on ingredients.
 	const ingredientSearchResponse = await fetchRecipeByIngredients(recipeByIngredientsUrl);
-	if (ingredientSearchResponse instanceof Response) {
+	if (!ingredientSearchResponse.ok) {
 		return ingredientSearchResponse;
 	}
 
+	// 4. Extract recipe IDs from the fetched recipes
 	const recipesData = await ingredientSearchResponse.json();
-	const { recipeIds } = extractRecipeIds(recipesData);
-
-	return new Response(JSON.stringify(ingredientSearchResponse), {
-		status: 200
-	});
+	const { errorResponse } = extractRecipeIds(recipesData);
+	if (errorResponse) {
+		return errorResponse;
+	}
 };
