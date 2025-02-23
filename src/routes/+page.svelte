6472a374cba39de49da25137596f7d14ee7a3daa
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-23 15:34:30 -0500

    green test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,24 @@
 <script>
 	import SearchButton from '$lib/SearchButton/SearchButton.svelte';
 	import SearchBar from '$lib/SearchBar/SearchBar.svelte';
+
+	let ingredients = '';
+
+	async function searchRecipes() {
+		try {
+			const response = await fetch(`/searchRecipes?ingredients=${ingredients}`);
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.error('Error fetching recipes:', error);
+		}
+	}
 </script>
 
 <div class="flex min-h-screen items-start justify-center bg-green-100 pt-20 font-serif text-4xl">
 	<div class="flex-col">
 		<h1>What is in your fridge?</h1>
-		<SearchBar />
-		<SearchButton />
+		<SearchBar bind:ingredients />
+		<SearchButton onClick={searchRecipes} />
 	</div>
 </div>
