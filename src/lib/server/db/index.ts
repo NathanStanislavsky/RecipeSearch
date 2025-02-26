5fec7c89586176e1bc369976fbeabeb69c8bc1f9
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-26 18:46:14 -0500

    schema and db migration

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,9 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
-import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = neon(env.DATABASE_URL);
-export const db = drizzle(client);
+import { drizzle, sql } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import { schema } from './schema';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,29 @@
-import { pgTable, serial, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
-	age: integer('age')
+	email: text('email').notNull().unique(),
+	password: text('password').notNull()
+});
+
+// stores only the recipes that users have favorited
+export const recipes = pgTable('recipes', {
+	id: serial('id').primaryKey(),
+	image: text('image').notNull(),
+	title: text('title').notNull(),
+	readyInMinutes: integer('ready_in_minutes').notNull(),
+	servings: integer('servings').notNull(),
+	sourceUrl: text('source_url').notNull()
+});
+
+// relational table that links users to their favorite recipes by IDs
+/*
+	Cascading ensures that when a user is deleted any favorites records referencing that user will be deleted
+	Same goes for recipes. When a recipe is deleted from recipes table then any reference to it from the favorites
+	table will also be deleted.
+*/
+export const favorites = pgTable('favorites', {
+	id: serial('id').primaryKey(),
+	userId: integer('user_id').references(() => users.id, { onDelete: 'cascade' }),
+	recipeId: integer('recipe_id').references(() => recipes.id, { onDelete: 'cascade' })
 });
