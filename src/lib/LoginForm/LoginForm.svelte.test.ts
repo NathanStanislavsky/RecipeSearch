c850ed5eadb514c84634f54cf51f12d5deb66f86
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-28 14:43:44 -0500

    red test - does not allow submission if fields are empty

--- src/lib/LoginForm/LoginForm.svelte.test.ts
+++ src/lib/LoginForm/LoginForm.svelte.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from 'vitest';
-import { render } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import LoginForm from './LoginForm.svelte';
+import { userEvent } from '@testing-library/user-event';
+
 
 describe('LoginForm', () => {
 	it('renders the email, password fields, and login button', () => {
@@ -9,4 +11,17 @@ describe('LoginForm', () => {
 		expect(getByLabelText(/password/i)).toBeInTheDocument();
 		expect(getByRole('button', { name: /login/i })).toBeInTheDocument();
 	});
+
+    it("does not allow submission if one or more fields are empty", async () => {
+        render(LoginForm);
+    
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getByLabelText(/password/i);
+        const loginButton = screen.getByRole("button", { name: /Login/i });
+    
+        await userEvent.click(loginButton);
+    
+        expect(emailInput).toBeInvalid();
+        expect(passwordInput).toBeInvalid();
+    });
 });
