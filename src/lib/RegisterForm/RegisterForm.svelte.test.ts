8678fd594e9172bd0362fa6a9a7bc7152d11e96e
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-02-28 15:11:02 -0500

    red test - does not allow submission if fields are empty

--- src/lib/RegisterForm/RegisterForm.svelte.test.ts
+++ src/lib/RegisterForm/RegisterForm.svelte.test.ts
@@ -12,4 +12,22 @@ describe('register form', () => {
         
 		expect(getByRole('button', { name: /Register/i })).toBeInTheDocument();
 	});
+
+    it('does not allow submission if one or more fields are empty', async () => {
+		const { getByLabelText, getByRole } = render(RegisterForm);
+
+        const usernameInput = getByLabelText(/username/i);
+		const emailInput = getByLabelText(/email/i);
+		const passwordInput = getByLabelText(/password/i);
+
+		const loginButton = getByRole('button', { name: /login/i });
+
+		// Setup the user event instance
+		const user = userEvent.setup();
+		await user.click(loginButton);
+
+        expect(usernameInput).toBeInvalid();
+		expect(emailInput).toBeInvalid();
+		expect(passwordInput).toBeInvalid();
+	});
 });
