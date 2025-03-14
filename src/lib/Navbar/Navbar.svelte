ee604038169e666b6aba7c634c1816676b863e38
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-03-14 15:05:47 -0400

    green test

---
+++ src/lib/Navbar/Navbar.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+	export let user = $page.data?.user;
+</script>
+
+<nav class="flex h-16 items-center border-b border-gray-200 bg-white px-6">
+	<div>
+		<span class="font-semibold text-gray-700">PantryChef</span>
+	</div>
+</nav>
