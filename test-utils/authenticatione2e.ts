457883a13cf0369d08ace849a1258dc8482ae64b
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-03-14 16:33:12 -0400

    updated helper test auth functions

--- test-utils/authenticatione2e.ts
+++ test-utils/authenticatione2e.ts
@@ -1,7 +1,6 @@
 import { Page } from '@playwright/test';
 
 export async function registerUser(page: Page, name: string, email: string, password: string) {
-	await page.goto('/register');
 	await page.fill('input[name="name"]', name);
 	await page.fill('input[name="email"]', email);
 	await page.fill('input[name="password"]', password);
@@ -10,7 +9,6 @@ export async function registerUser(page: Page, name: string, email: string, pass
 }
 
 export async function loginUser(page: Page, email: string, password: string) {
-	await page.goto('/login');
 	await page.fill('input[name="email"]', email);
 	await page.fill('input[name="password"]', password);
 	await page.click('button[type="submit"]');
