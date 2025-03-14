e30d54908146ce8faa2272fffa1cf127e36238d7
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-03-14 16:33:20 -0400

    green test

--- src/lib/Navbar/Navbar.svelte
+++ src/lib/Navbar/Navbar.svelte
@@ -3,10 +3,11 @@
 	export let user = $page.data?.user;
 </script>
 
-<nav class="flex h-16 items-center justify-between border-b border-gray-200 bg-white px-6">
-	<div>
+<nav class="fixed top-0 left-0 right-0 z-50 flex h-16 items-center border-b border-gray-200 bg-white px-6 font-serif">
+	<!-- Left Column -->
+	<div class="flex-1">
 		{#if !user}
-			<a href="/signup">
+			<a href="/register">
 				<button class="rounded border border-gray-300 px-4 py-2 text-gray-700 hover:bg-gray-50">
 					Register
 				</button>
@@ -14,13 +15,17 @@
 		{/if}
 	</div>
 
-	<div class="flex items-center space-x-2">
+	<!-- Center Column -->
+	<div>
 		<span class="font-semibold text-gray-700">PantryChef</span>
 	</div>
 
-	<div class="flex items-center space-x-6">
+	<!-- Right Column -->
+	<div class="flex-1 flex justify-end items-center">
 		{#if !user}
-			<a href="/signin" class="text-gray-700 hover:text-gray-900"> Sign in </a>
+			<a href="/login" class="text-gray-700 hover:text-gray-900">
+				Sign in
+			</a>
 		{/if}
 	</div>
 </nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Navbar from '$lib/Navbar/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<main
+	class="flex flex-col items-start justify-center min-h-screen bg-cover bg-center p-8"
+	style="background-image: url('https://m.media-amazon.com/images/I/71VgmMSxYWL.jpg')"
+>
+	<div class="max-w-3xl font-serif">
+		<h1 class="text-8xl font-bold mb-4 text-white">Welcome to PantryChef</h1>
+		<p class="text-2xl mb-8 text-white">
+			To search for recipes, please register and log in. Once you're signed in, you'll be able to
+			find and save your favorite recipes.
+		</p>
+	</div>
+</main>
