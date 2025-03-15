b9f94cc4ff3105cab4df924405b253fdd9f443fb
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-03-15 17:46:49 -0400

    updated background colors

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,7 +2,9 @@
 	import LoginForm from '$lib/LoginForm/LoginForm.svelte';
 </script>
 
-<div class="flex min-h-screen items-center justify-center">
+<div
+	class="flex min-h-screen items-center justify-center bg-slate-100"
+>
 	<div class="w-full max-w-lg">
 		<LoginForm />
 	</div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,7 +2,7 @@
 	import RegisterForm from '$lib/RegisterForm/RegisterForm.svelte';
 </script>
 
-<div class="flex min-h-screen items-center justify-center">
+<div class="flex min-h-screen items-center justify-center bg-slate-100">
 	<div class="w-full max-w-lg">
 		<RegisterForm />
 	</div>
