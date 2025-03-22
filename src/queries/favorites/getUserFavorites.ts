d0afe6046d9518921ff2bf701824effb4798422a
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-03-21 23:06:27 -0400

    get all of user's favorite recipes query

---
+++ src/queries/favorites/getUserFavorites.ts
@@ -0,0 +1,19 @@
+import { db } from '$lib/server/db/index.ts';
+import { recipes, favorites } from '$lib/server/db/schema.ts';
+import { eq } from 'drizzle-orm';
+
+async function getUserFavorites(userId: number) {
+	const favoriteRecipes = await db
+		.select({
+			recipeId: recipes.id,
+			title: recipes.title,
+			image: recipes.image,
+			readyInMinutes: recipes.readyInMinutes,
+			servings: recipes.servings,
+			sourceUrl: recipes.sourceUrl
+		})
+		.from(favorites)
+		.innerJoin(recipes, eq(favorites.recipeId, recipes.id))
+		.where(eq(favorites.userId, userId));
+	return favoriteRecipes;
+}
