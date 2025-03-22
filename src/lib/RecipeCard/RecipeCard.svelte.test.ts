079c063b5b9b79b635045fd8b33b46d91d2c4367
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-03-22 17:16:44 -0400

    red test - star button

--- src/lib/RecipeCard/RecipeCard.svelte.test.ts
+++ src/lib/RecipeCard/RecipeCard.svelte.test.ts
@@ -30,4 +30,23 @@ describe('RecipeCard', () => {
 			recipe.sourceUrl
 		);
 	});
+
+	it('Star exists and is unfilled by default', () => {
+		// Find the star button
+		const starButton = screen.getByRole('button');
+		expect(starButton).toBeInTheDocument();
+
+		// Find the icon via its test id
+		const icon = screen.getByTestId('favorite-icon');
+		expect(icon).toBeInTheDocument();
+
+		// Query for the <path> element within the SVG
+		const pathElement = icon.querySelector('svg path');
+		expect(pathElement).toBeTruthy();
+		if (pathElement) {
+			expect(pathElement.getAttribute('d')).toContain(
+				'm12 15.39l-3.76 2.27l.99-4.28l-3.32-2.88l4.38-.37L12 6.09l1.71 4.04l4.38.37l-3.32 2.88l.99 4.28M22 9.24l-7.19-.61L12 2L9.19 8.63L2 9.24l5.45 4.73L5.82 21L12 17.27L18.18 21l-1.64-7.03L22 9.24Z'
+			);
+		}
+	});
 });
