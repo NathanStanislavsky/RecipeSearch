cbc05409a758dc2c9d65000d45c644b7264b487d
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-04-15 16:27:19 -0400

    updated deprecated function

--- src/utils/test/authenticationUtils.ts
+++ src/utils/test/authenticationUtils.ts
@@ -5,12 +5,12 @@ export async function registerUser(page: Page, name: string, email: string, pass
 	await page.fill('input[name="email"]', email);
 	await page.fill('input[name="password"]', password);
 	await page.click('button[type="submit"]');
-	await page.waitForNavigation();
+	await page.waitForURL(/.*login/);
 }
 
 export async function loginUser(page: Page, email: string, password: string) {
 	await page.fill('input[name="email"]', email);
 	await page.fill('input[name="password"]', password);
 	await page.click('button[type="submit"]');
-	await page.waitForNavigation();
+	await page.waitForURL(/.*search/);
 }
