f1091626360c9b6c0c47b64a2538793f97bd2991
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-05-25 12:31:44 -0400

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 	<div class="max-w-3xl font-serif">
 		<h1 class="mb-4 text-8xl font-bold text-white">Welcome to PantryChef</h1>
 		<p class="mb-8 text-2xl text-white">
-			To search for recipes, please register and log in. Once you're signed in, you'll be able to
+			To search for recipes, please register and log in. Once you're logged in, you'll be able to
 			find and save your favorite recipes.
 		</p>
 	</div>
