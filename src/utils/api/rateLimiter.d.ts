57cdc2e683b7ecdc4b5feea410bc2e30e980e872
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-06-22 01:01:59 -0400

    removed old api code

--- src/utils/api/rateLimiter.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class RateLimiter {
-	private static getTodayRecord;
-	private static createTodayRecord;
-	private static incrementCounter;
-	static checkAndIncrement(): Promise<boolean>;
-}
