531ff9af25e63104d3f4f32382825149beb12620
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-07-28 21:36:15 -0400

    update eslint

--- services/web/eslint.config.js
+++ services/web/eslint.config.js
@@ -1,20 +1,17 @@
 import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
-import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
-	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
 	prettier,
 	...svelte.configs['flat/prettier'],
 	{
+		files: ['src/**/*.{js,ts,svelte}'],
 		languageOptions: {
 			globals: {
 				...globals.browser,
@@ -23,12 +20,22 @@ export default ts.config(
 		}
 	},
 	{
-		files: ['**/*.svelte'],
-
+		files: ['src/**/*.svelte'],
 		languageOptions: {
 			parserOptions: {
 				parser: ts.parser
 			}
 		}
+	},
+	{
+		ignores: [
+			'.svelte-kit/**',
+			'.vercel/**',
+			'node_modules/**',
+			'dist/**',
+			'build/**',
+			'*.config.js',
+			'*.config.ts'
+		]
 	}
 );
