4e4f4b4f5b1d4089e18aeb1a5c9d0278bcfc5616
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-09-27 12:54:57 -0400

    cleaned up postgre database

--- services/web/src/data/database/schema.ts
+++ services/web/src/data/database/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
@@ -7,39 +7,4 @@ export const users = pgTable('users', {
 	password: text('password').notNull()
 });
 
-// stores only the recipes that users have favorited
-export const recipes = pgTable('recipes', {
-	id: serial('id').primaryKey(),
-	image: text('image').notNull(),
-	title: text('title').notNull(),
-	readyInMinutes: integer('ready_in_minutes').notNull(),
-	servings: integer('servings').notNull(),
-	sourceUrl: text('source_url').notNull()
-});
-
-// relational table that links users to their favorite recipes by IDs
-/*
-	Cascading ensures that when a user is deleted any favorites records referencing that user will be deleted
-	Same goes for recipes. When a recipe is deleted from recipes table then any reference to it from the favorites
-	table will also be deleted.
-*/
-export const favorites = pgTable('favorites', {
-	id: serial('id').primaryKey(),
-	userId: integer('user_id').references(() => users.id, { onDelete: 'cascade' }),
-	recipeId: integer('recipe_id').references(() => recipes.id, { onDelete: 'cascade' })
-});
-
-export const apiRequests = pgTable('api_requests', {
-	id: serial('id').primaryKey(),
-	date: text('date').notNull().unique(),
-	count: integer('count').notNull().default(0)
-});
-
-export const ingredientSearches = pgTable('ingredient_searches', {
-	id: serial('id').primaryKey(),
-	ingredients: text('ingredients').notNull().unique(),
-	recipeIds: text('recipe_ids').notNull(),
-	createdAt: timestamp('created_at').defaultNow()
-});
-
-export const schema = { users, recipes, favorites, apiRequests };
+export const schema = { users };
