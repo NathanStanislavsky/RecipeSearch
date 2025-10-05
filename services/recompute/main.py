3f933089f629108e8217108ac52c7cdb8b726625
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-10-04 23:31:01 -0400

    process_rating_event init

--- services/recompute/main.py
+++ services/recompute/main.py
@@ -29,7 +29,26 @@ async def handle_rating_event(request: Request):
         raise HTTPException(500, f"Error recomputing: {str(e)}")
 
 async def process_rating_event(rating_event: dict):
-    pass
+    user_id = rating_event['user_id']
+    recipe_id = rating_event['recipe_id']
+    rating = rating_event['rating']
+    rating_timestamp = datetime.fromtimestamp(rating_event['timestamp'])
+
+    if not all([user_id, recipe_id, rating, rating_timestamp]):
+        print(f"Invalid rating event: {rating_event}")
+        return
+    
+    # Retrieve the last SVD completion time and if the rating_timestamp is older than
+    # that, then we don't need to recompute since it's already been incorporated by SVD
+    # we sack the message and return
+
+    # If the timestamp check passes then we need to recompute the user vector by first
+    # getting the user vector from the PostgreSQL database and the recipe vector from GCS
+    
+    # Then we apply the weighted average formula on the user vector
+
+    # Finally we update the user vector in the PostgreSQL database
+    
 
 @app.get("/health")
 def health():
