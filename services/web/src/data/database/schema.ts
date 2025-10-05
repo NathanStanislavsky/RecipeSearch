989d213737b52c83052894f72346da2399635f5f
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-10-04 23:03:53 -0400

    added user_vector and svd_metadata tables

--- services/web/src/data/database/schema.ts
+++ services/web/src/data/database/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp, integer, real } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
@@ -7,4 +7,16 @@ export const users = pgTable('users', {
 	password: text('password').notNull()
 });
 
-export const schema = { users };
+export const user_vectors = pgTable('user_vectors', {
+	user_id: integer('user_id').primaryKey().references(() => users.id),
+	vector: real('vector').array().notNull(),
+	updated_at: timestamp('updated_at', { withTimezone: true }).notNull().defaultNow(),
+});
+
+export const svd_metadata = pgTable('svd_metadata', {
+	id: serial('id').primaryKey(),
+	completion_time: timestamp('completion_time', { withTimezone: true }).notNull(),
+	created_at: timestamp('created_at', { withTimezone: true }).notNull().defaultNow()
+});
+
+export const schema = { users, user_vectors, svd_metadata };
